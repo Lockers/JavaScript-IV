620523762e493a920cad8030e54377bdb3110190
Matt Locklin <mattlocklin@live.co.uk>
2019-05-09 15:07:13 +0100

    Finished MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,12 +10,12 @@ class Person {
     }
 }
 
-// const Matt = new Person({
-//     name: 'Matt',
-//     age: 37,
-//     location: 'The Moon',
-//     gender: 'Neutral'
-// })
+const Matthew = new Person({
+    name: 'Matt',
+    age: 37,
+    location: 'The Moon',
+    gender: 'Neutral'
+})
 
 class Instructor extends Person {
     constructor(object) {
@@ -27,8 +27,8 @@ class Instructor extends Person {
     demo(subject) {
         return `Today we are learning about ${subject}`
     }
-    grade(Student, subject) {
-        `${Student.name} recieves a perfect score on ${subject}`
+    grade(student, subject) {
+        return `${student.name} recieves a perfect score on ${subject}`
     }
 }
 
@@ -68,3 +68,29 @@ const Matt = new Student({
     className: 'Fairies',
     favSubjects: ['HTML', ' CSS ', ' Javascript']
 })
+
+class ProjectManager extends Instructor {
+    constructor(object) {
+        super(object);
+        this.gradClassName = object.gradClassName;
+        this.favInstructor = object.favInstructor;
+    }
+    standUp(slackChannel) {
+        return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}.`
+    }
+}
+
+const Borja = new ProjectManager({
+    name: 'Borja',
+    age: 435,
+    location: 'Parts Unknown',
+    gender: 'Male',
+    speciality: 'Knives',
+    catchphrase: 'So you think your a Dev?',
+    gradClassName: 'EU2',
+    favInstructor: 'God'
+})
+
